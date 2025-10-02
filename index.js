f62b9e3a266f1833e69ff59582f68ae7181ce076
Cosmin <cbuzuloiu@gmail.com>
2025-10-02 22:16:07 +0300

    project setup

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(port, () => {
+  console.log(`Successfully started server on port ${port}.`);
+});
